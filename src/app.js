034edff42681a20052244c78bc36fd1218c41b63
Proc3ssa <pros3sa@gmail.com>
2026-06-17 21:04:36 +0000

    update: add new routes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const authRoutes = require("./routes/auth.routes");
+const eventRoutes = require("./routes/event.routes");
 
 const app = express();
 
@@ -10,6 +11,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+
+app.use("/api/events", eventRoutes);
+app.use("/uploads", express.static("uploads"));
 
 app.get("/", (req, res) => {
   res.send("Eventify API running");
