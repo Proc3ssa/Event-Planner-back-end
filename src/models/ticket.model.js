50cbabf7986f50dac7a11c14e1eb5efc7cdcf890
Proc3ssa <pros3sa@gmail.com>
2026-08-02 21:00:44 +0000

    feat: add ticket model with CRUD operations for ticket management

---
+++ src/models/ticket.model.js
@@ -0,0 +1,57 @@
+const db = require("../config/db");
+
+const createTicket = ({ event_id, token, recipient_name, recipient_contact, contact_type }) => {
+  return new Promise((resolve, reject) => {
+    db.query(
+      "INSERT INTO tickets (event_id, token, recipient_name, recipient_contact, contact_type) VALUES (?, ?, ?, ?, ?)",
+      [event_id, token, recipient_name, recipient_contact, contact_type],
+      (err, result) => {
+        if (err) return reject(err);
+        resolve({ id: result.insertId, token });
+      }
+    );
+  });
+};
+
+const getTicketByToken = (token) => {
+  return new Promise((resolve, reject) => {
+    db.query(
+      `SELECT t.*, e.name as event_name, e.date, e.time, e.venue, e.type, e.flyer
+       FROM tickets t JOIN events e ON t.event_id = e.id
+       WHERE t.token = ?`,
+      [token],
+      (err, results) => {
+        if (err) return reject(err);
+        resolve(results[0]);
+      }
+    );
+  });
+};
+
+const markTicketUsed = (token) => {
+  return new Promise((resolve, reject) => {
+    db.query(
+      "UPDATE tickets SET status = 'used' WHERE token = ?",
+      [token],
+      (err) => {
+        if (err) return reject(err);
+        resolve();
+      }
+    );
+  });
+};
+
+const getTicketsByEvent = (event_id) => {
+  return new Promise((resolve, reject) => {
+    db.query(
+      "SELECT * FROM tickets WHERE event_id = ? ORDER BY created_at DESC",
+      [event_id],
+      (err, results) => {
+        if (err) return reject(err);
+        resolve(results);
+      }
+    );
+  });
+};
+
+module.exports = { createTicket, getTicketByToken, markTicketUsed, getTicketsByEvent };
