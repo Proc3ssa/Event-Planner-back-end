21ebde23c632455a9b2f331cc1d0dfaf4a3aa243
Proc3ssa <pros3sa@gmail.com>
2026-08-10 19:54:34 +0000

    config: set up routing for all trafficks

--- src/app.js
+++ src/app.js
@@ -11,7 +11,12 @@ const ticketRoutes = require("./routes/ticket.routes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
